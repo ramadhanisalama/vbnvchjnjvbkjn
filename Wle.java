ee1a6328574a041e8fc136b26487e83227c1e3fe
ramadhanisalama <95014953+ramadhanisalama@users.noreply.github.com>
2021-12-10 09:28:04 -0800

    Add files via upload

---
+++ Wle.java
@@ -0,0 +1,10 @@
+public class Wle{
+    public static void main(String[]args){
+        int i = 10;
+        while(i>10){
+            System.out.println(i);
+            i --;
+
+        }
+    }
+}
