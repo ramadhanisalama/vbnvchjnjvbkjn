bc68c6d19065675e3eb809c06551336e0c341bd0
ramadhanisalama <95014953+ramadhanisalama@users.noreply.github.com>
2021-12-10 10:40:54 -0800

    Add files via upload

---
+++ P2one.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class P2one{
+    public static void main(String[]args){
+        int x = 10;
+        int y =5;
+        int z = 20;
+        if(10 >  5)
+         System.out.println(x + "is greater");
+        else {
+            System.out.println(z + " is greater");
+        }
+    }
+}
