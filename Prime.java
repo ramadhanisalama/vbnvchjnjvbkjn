0cd16b9e83fd4c5e1f99469d5192fd52888488df
ramadhanisalama <95014953+ramadhanisalama@users.noreply.github.com>
2021-12-10 10:45:07 -0800

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,10 @@
+public class Prime{
+    public static void main(String[]args){
+        int n = 10;
+        for(int i = 1 ; i <= n ; i ++){
+            if(i % 2 == i $$ i <= i){
+                System.out.println(i + "");
+            }
+        }
+    }
+}
