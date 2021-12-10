9353c748602dc73827bea79c33e1f03809a518ea
ramadhanisalama <95014953+ramadhanisalama@users.noreply.github.com>
2021-12-10 07:37:46 -0800

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,6 @@
+public class Word{
+    public static void main (String[]args){
+        System.out.println("Tomorow I will learn java data types:");
+        System.out.println("This is bad command:delc:/*.*");
+    }
+}
